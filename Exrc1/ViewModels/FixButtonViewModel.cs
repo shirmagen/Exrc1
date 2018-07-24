d2e073074a35fc6bb4bc50efa96e14cffb23f8c0
Rick Morty <a2743061@nwytg.net>
2018-07-24 17:34:46 +0300

    feat(exrc1): fixed caliburn and added disable

--- Exrc1/ViewModels/FixButtonViewModel.cs
+++ Exrc1/ViewModels/FixButtonViewModel.cs
@@ -4,21 +4,35 @@ namespace Exrc1.ViewModels
 {
     public class FixButtonViewModel : PropertyChangedBase
     {
-        private int _bugsCounter = 0;
+        private int _bugsCounter;
 
         public int BugsCounter
         {
-            get { return _bugsCounter; }
+            get
+            {
+                return _bugsCounter;
+            }
             set
             {
                 _bugsCounter = value;
                 NotifyOfPropertyChange(() => BugsCounter);
+                NotifyOfPropertyChange(() => CanIncreaseBugs);
             }
         }
 
-        public void IncreaseCounter()
+        public bool CanIncreaseBugs
         {
+            get { return BugsCounter < 30; }
+        }
 
+        public FixButtonViewModel()
+        {
+            BugsCounter = 0;
+        }
+
+        public void IncreaseBugs()
+        {
+            BugsCounter++;
         }
     }
 }
