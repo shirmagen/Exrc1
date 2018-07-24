d54296903bcdc8c80d6e09e8861449f2dbfad9ef
Rick Morty <a2743061@nwytg.net>
2018-07-24 14:58:16 +0300

    feat(): added basic view and view model

---
+++ Exrc1/AppBootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using Exrc1.ViewModels;
+using System.Windows;
+
+namespace Exrc1
+{
+    public class AppBootstrapper : BootstrapperBase
+    {
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<FixButtonViewModel>();
+        }
+    }
+}
---
+++ Exrc1/ViewModels/FixButtonViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+
+namespace Exrc1.ViewModels
+{
+    public class FixButtonViewModel : PropertyChangedBase
+    {
+        private int _bugsCounter = 0;
+
+        public int BugsCounter
+        {
+            get { return _bugsCounter; }
+            set
+            {
+                _bugsCounter = value;
+                NotifyOfPropertyChange(() => BugsCounter);
+            }
+        }
+
+        public void IncreaseCounter()
+        {
+
+        }
+    }
+}
